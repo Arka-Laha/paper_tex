f851a02e2ea049ed212cc9005adc98faa0dc3c53
neurocoderdev <neurocoder.dev@gmail.com>
2026-06-28 03:42:50 +0530

    Highlights Page Control

---
+++ highlights-enable.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# Uncomments the highlights block in both cas-dc-template.tex and Sectioned/main.tex
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+FILES=(
+    "$SCRIPT_DIR/cas-dc-template.tex"
+    "$SCRIPT_DIR/Sectioned/main.tex"
+)
+
+for FILE in "${FILES[@]}"; do
+    sed -i \
+        -e 's/^%\\begin{highlights}/\\begin{highlights}/' \
+        -e 's/^%    \\item Research highlights item /    \\item Research highlights item /' \
+        -e 's/^%\\end{highlights}/\\end{highlights}/' \
+        "$FILE"
+    echo "Highlights enabled in: $FILE"
+done
